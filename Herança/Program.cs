9fc1231183000f8a5bb4ddc58c9951b850f01550
Emerson <emersonlapa@outlook.com>
2020-11-24 07:16:32 -0300

    Upload

---
+++ Herança/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Herança.Entities;
+
+namespace Herança
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brow", 100.0, 500.0);
+            Console.WriteLine(account.Balance);
+           // account.Balance = 200;
+        }
+    }
+}
